22afe25b657bf1a873d4e1f6153591159fe3637d
Tiago <tiago.kamegasawa93@gmail.com>
2019-11-06 01:19:49 -0300

    Teste unitario do validator

---
+++ src/test/java/br/com/fiap/usuarios/validator/UserValidatorTest.java
@@ -0,0 +1,39 @@
+package br.com.fiap.usuarios.validator;
+
+import static org.mockito.Mockito.when;
+
+import br.com.fiap.usuarios.entity.User;
+import br.com.fiap.usuarios.repository.user.UserRepository;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class UserValidatorTest {
+
+    @Mock
+    private UserRepository userRepository;
+
+    @InjectMocks
+    private UserValidator validator;
+
+    @Rule
+    public ExpectedException expectedException = ExpectedException.none();
+
+    @Test
+    public void validateInsert() {
+        User entity = User.builder().cpfCnpj("12345678911").email("gabriel.gabes@custodia.com.br").build();
+        when(userRepository.existWithCpfCnpjOrEmail(entity.getCpfCnpj(),
+                                                    entity.getEmail())).thenReturn(true);
+        expectedException.expectMessage("Já existe outro usuário cadastrado com o mesmo cnpj ou email");
+
+        this.validator.validateInsert(entity);
+
+    }
+}
+
+
